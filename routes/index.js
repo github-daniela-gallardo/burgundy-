6206e9dc6fa2af7427237a7b3f9f60b5cd7cdbdd
Esther Liriano <estherlirianor@gmail.com>
2022-11-22 15:18:52 -0500

    esthercommit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/landing', (req, res, next) => {
+  res.render('landing')
+})
+
 module.exports = router;
